1b65c1cfbb45c3acd97c67b2caf03ec0e0a59b35
JoyDTechGirl <samsonjoyuwaoma7@gmail.com>
2025-03-09 04:33:31 +0100

    i made a change now

--- Validation/user.js
+++ Validation/user.js
@@ -1,8 +1,8 @@
 const joi = require('joi')
 
 exports.registerUserSchema = joi.object().keys({
-  fullName: joi.string().min(3).max(10).required(),
-  email: joi.string().trim().email().max(8).required(),
+  fullName: joi.string().min(3).max(15).required(),
+  email: joi.string().trim().email().max(29).required(),
   password: joi.string().trim().required(),
   gender: joi.string().trim().valid('Male','Female').required()
 });
